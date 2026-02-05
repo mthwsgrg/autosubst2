eda8fe3fc5d71b059d190768f49bb9083306b74c
Mathews George <mathewsgeorge1997@gmail.com>
2026-02-05 20:07:32 +0000

    reduction laws gen proto done

--- Src/Autosubst/GenTactic.hs
+++ Src/Autosubst/GenTactic.hs
@@ -17,38 +17,71 @@ import           Data.List                as L
 -- Generation of as_apply tactic
 
 
-
 genAsApply :: [TId] -> GenM [Sentence]
 genAsApply xs = return $ [SentenceId "(** as_apply follows **)"]
 
 
 
 
+genRedLawsCons :: TId -> Constructor -> GenM TacticEquation
+genRedLawsCons x (Constructor pms name pos) = do
+  subSorts <- substOfSorts x
+  argSorts <- arguments x
+  if checkBinder pos then do_something
+  else
+    let s = genPatternNames "?s" pos in
+    let sigma = genNames "?sigma" subSorts in
+    let subTerms = genSubTerms (zip s argSorts) (zip sigma subSorts) in    
+    return $ TacticEquation $ (TacticPatternTerm $ JustTerm $ idApp s subTerms) $ TacticId "whatever"
+
+
+-- a filter function to filter from subSorts
 
 
 
+genNames :: String -> [a] -> [String]      
+
+genRedLaws :: [TId] -> GenM [TacticEquation]
 
 
 
+genRedLaws varSorts = do
+   csList <- something_happens
+   let cons_pattern (Constructor pms name pos) =
+          (if (checkBinder pos) then do_something
+         else
+           let s = genPatternNames "?s" pos in
+           let termsTIds = zip s and tids in
+           let subsTIds = zip subnames and tids) in
+     
+   return $ idApp name           
+           
+
+     
+   
+   
+
+-- A list of pair of substitutions/renamings with corresponding sorts
+
+type SubRenVector = [(Term, TId)]
 
 
+checkBinder :: Constructor -> Bool
 
 
+-- Takes a sort id, sub/ren vector and returns a subset of it that only appears in the sub/ren vector of Id 
+subRenVecSort :: TId -> SubRenVector -> SubRenVector
 
 
-
-
-
-
-
-
-
+substOfSorts :: TId -> GenM [TId]
+substOfSorts = return $ substOf TId
 
 
 
 
 -- Generation of program data to see what's happening
 
+{-
 termStr :: Term -> String
 
 termStr (TermId s) = s
@@ -69,5 +102,5 @@ genBottomJunk varSorts xs substSorts upList = do
 
 genmString :: (Show a) =>  a -> GenM String
 genmString x = return (show x)
-
+-}
 
